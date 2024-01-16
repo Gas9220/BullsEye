fec9959523b62b683ebe8d4380b69741ebae2d9b
Gaspare <Gas9220@users.noreply.github.com>
2024-01-16 11:47:26 +0100

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Gaspare Monte on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
