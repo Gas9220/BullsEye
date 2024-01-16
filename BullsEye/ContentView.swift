239dfa79eecaa123e6a3d56c7857499ff73f389b
Gaspare <Gas9220@users.noreply.github.com>
2024-01-16 11:56:27 +0100

    feat: create UI for generated color and guess color

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -10,12 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Color(red: 0.5, green: 0.5, blue: 0.5)
+            Text("R: ??? G: ??? B: ???")
+                .padding()
+
+            Color(red: 0.5, green: 0.5, blue: 0.5)
+            Text("R: 204 G: 76 B: 178")
+                .padding()
+
+            Slider(value: .constant(0.5))
+
+            Button("Hit me!") {
+
+            }
         }
-        .padding()
     }
 }
 
